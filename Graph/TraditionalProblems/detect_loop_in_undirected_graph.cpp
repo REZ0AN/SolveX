d902a22355238dc22a5a1c00bdec5a053fd1e450
REZ-OAN <ahmedabir.rez@gmail.com>
2025-02-20 22:35:58 +0600

    implemented code for loop detection in an undirected graph

---
+++ Graph/TraditionalProblems/detect_loop_in_undirected_graph.cpp
@@ -0,0 +1,41 @@
+#include<iostream>
+#include<vector>
+#include<utility>
+
+using namespace std;
+
+bool isLoop = false;
+
+void dfs(vector<vector<int>> &graph, vector<bool> &visited, int start, int parent) {
+    visited[start]= true;
+    for(auto &child : graph[start]){
+        isLoop = isLoop || (parent != child && visited[child]);
+        if(!visited[child]){
+            dfs(graph, visited, child, start);
+        }
+    }
+    return;
+}
+
+int main() {
+    int n, m;
+    cin>>n>>m;
+    vector<vector<int>> graph(n+1);
+    for(int i = 0; i<m; i++){
+        int u, v;
+        cin>>u>>v;
+        graph[u].push_back(v);
+        if(v != u){
+            graph[v].push_back(u);
+        }
+
+    }
+    vector<bool>visited(n,false);
+
+    dfs(graph, visited, 1, 0);
+    if(isLoop){
+        cout<<"YES"<<endl;
+    }else {
+        cout<<"NO"<<endl;
+    }
+}
