cbfdd4801063f17ca00e329cac016aea827ab017
REZ-OAN <ahmedabir.rez@gmail.com>
2025-02-21 16:25:11 +0600

    implement bfs using queue

---
+++ Graph/Traversal/bfs.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+
+using namespace std;
+
+void bfs(int start, const vector<vector<int>>& graph) {
+    vector<bool> visited(graph.size(), false);
+    queue<int> q;
+
+    visited[start] = true;
+    q.push(start);
+
+    while (!q.empty()) {
+        int node = q.front();
+        q.pop();
+        cout << node << " ";
+
+        for (int neighbor : graph[node]) {
+            if (!visited[neighbor]) {
+                visited[neighbor] = true;
+                q.push(neighbor);
+            }
+        }
+    }
+}
+
+int main() {
+    int n, m;
+    cin>>n>>m;
+
+    vector<vector<int>> graph(n+1);
+
+    for(int i = 0; i<m; i++){
+        int u, v;
+        cin>>u>>v;
+        graph[u].push_back(v);
+        if(v != u){
+            graph[v].push_back(u);
+        }
+
+    }
+
+    int startNode = 1;
+    
+    bfs(startNode, graph);
+    cout << endl;
+
+    return 0;
+}
