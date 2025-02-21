276fb283cf3e526db42d960af75a81c3132d1cb7
REZ-OAN <ahmedabir.rez@gmail.com>
2025-02-21 20:36:10 +0600

    add pair uses

---
+++ STL/pairs/pairs.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+#include<utility>
+#include<string>
+using namespace std;
+
+int main() {
+    
+    // declaration pair<type, type> name;
+    pair<int, string> p={3,"HelloWorld"};
+    cout<<p.first<<" "<<p.second<<endl;
+}
